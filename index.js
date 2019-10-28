80cd01a6cab299047000e256c20bc3312ee8b44c
github username <github email address>
2019-10-28 14:55:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+// // Your code here
+// document.addEventListener("keydown", function(e) {
+//   console.log(e.key);
+// });
+
+// document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft") {
+//     let leftNumbers = dodger.style.left.replace("px", "");
+//     let left = parseInt(leftNumbers, 10);
+ 
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// })
+
+
+let dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
